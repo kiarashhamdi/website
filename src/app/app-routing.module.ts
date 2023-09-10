e431c5093068740281977926e13a9967f671ea0b
kiarashhamdi <kiarashhamdi@gmail.com>
2023-09-10 12:06:29 +0330

    add a simple route for cheking if it works in website

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HelloComponent } from './modules/hello/hello.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"hello",
+    component: HelloComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
